7615ad311db60f21385fa9575184c960ee298ad5
Maurício <mauriciof2408@gmail.com>
2022-04-14 07:04:45 -0300

    Adicionada animação na tabela, e efeitos na página home.

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,34 @@
-const botao = document.querySelectorAll('.cabecalho-botoes');
+const searchHide = document.querySelectorAll('.searchHide');
+const botao = document.querySelectorAll('.tabela-botao');
+let trigger = 0;
 
-for( let i = 0; i < botao.length; i++){
-    botao[i].onclick = tocaSom;
+if ( trigger === 1){
+    hideTable();
+}
+else{
+    showTable();
 }
 
-function tocaSom(){
-    document.querySelector('#dbz').play();
+function hideTable(){
+    for( let i =0; i < searchHide.length; i++){
+        botao.onclick = searchHide[i].classList.add('hide');
+    }
+
+    trigger = 0;
 }
 
+function showTable(){
+    for( let i = 0; i < botao.length; i++){
+        botao[i].onclick = showContent;
+    }
+    
+    const tableDatas = document.querySelectorAll('.hide');
+    
+    function showContent(){
+        for( let i = 0; i < searchHide.length; i++){
+            searchHide[i].classList.remove('hide');
+        }
+    }
 
+    trigger = 1;
+}
